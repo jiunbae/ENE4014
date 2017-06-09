2fc335ec253ace618078f86b5da874b57cfb1650
MaybeS <maytryark@gmail.com>
2017-06-10 05:33:34 +0900

    HW2 coin haskell

---
+++ Homework2/coin.hs
@@ -0,0 +1,18 @@
+import Data.List
+
+flipStack :: Int -> [Bool] -> [Bool]
+flipStack n xs = count `take` xs ++ reverse ((\x -> not x) `map` (drop count xs))
+                    where count = length xs - n
+
+flipRec :: [Bool] -> [Int]
+flipRec coins
+    | all (==True) coins = [0]
+    | otherwise          = count : flipRec (count `flipStack` coins)
+                            where count = if last coins
+                                    then 1 
+                                    else case (False `elemIndex` coins) of
+                                        Just n -> length coins - n
+                                        Nothing -> 0
+
+flipCoin :: [Char] -> [Int]
+flipCoin coins = flipRec (reverse ((\x -> x == 'H') `map` coins))
